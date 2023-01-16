77bb425202575f2e67440b62e783918f725b5bab
jancypp <120283327+jancypp@users.noreply.github.com>
2023-01-16 12:15:16 -0700

    fixed userInput function and object

--- script.js
+++ script.js
@@ -18,67 +18,90 @@
 var generateBtn = document.querySelector("#generate");
 
 //Arrays for password choices/
-const lowerArr = "abcdefghijklmnopqrstuvwxyz"; //.split a string
-const lowerCase = lowerArr.split(" " , 26)
-console.log(lowerCase);
+const lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(lowerArr);
 const numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 console.log(numberArr);
-const upperArr = ["A", "B", "C"];
+const upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+console.log(upperArr);
 const specialArr = ["!", "@", "#", "$", "^", "&", "*", "(", ")"];
+console.log(specialArr);
 
-// Write password to the #password input
+
+// starter code Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
 //taking the answers to process a password/
 function generatePassword() {
-  let userAnswer = userInput ()
+  let userAnswer = userInput()
 }
 
 
-
-// var results [];
-
-// User Input
-//How many characters would you like your password to contain?
-//Lower confirm
-//nums confirm
-//upperCase confirm
-//special confirm
+// User Input - How many characters would you like your password to contain? Confirms for each character type
 function userInput() {
-  let length = prompt ("How many characters would you like your password to contain?")
+  let length = prompt("How many characters would you like your password to contain? Please choose between 8 - 128 characters.")
   if (length < 8 || length > 128) {
-    alert ("Choose a length of at least 8 characters and no more than 128 characters")
+    alert("Error: Choose a length of at least 8 characters and no more than 128 characters")
     return null
   }
-  let islowerCase = confirm ("Do you want to include lowercase characters?")
-  let isupperCase = confirm ("Do you want to include uppercase characters?")
-  let isnumeric = confirm ("Do you want to include numbers characters?")
-  let isspecialChar = confirm ("Do you want to include special characters?")
+  let islowerCase = confirm("Do you want to include lowercase characters?")
+  let isupperCase = confirm("Do you want to include uppercase characters?")
+  let isnumeric = confirm("Do you want to include numbers characters?")
+  let isspecialChar = confirm("Do you want to include special characters?")
+  if (islowerCase === false && isupperCase === false && isnumeric === false && isspecialChar === false) {
+    alert("You must choose at least one character type to proceed.")
+    return null
+  }
+  let userOptions = {
+    length: length,
+    islowerCase: islowerCase,
+    isupperCase: isupperCase,
+    isspecialChar: isspecialChar,
+    isnumeric: isnumeric
+  }
+  return userOptions;
 }
-//Conditional
-//if user chose X number of charter total
-// for loop iterates X number of times
-
-//if the user chose to add lower case characters
-// randomly select some number represented by Y number of chars and pushed to the results array
-
-//if the users chose to add uppercase chars
-// randomly select some number represented by Y number of chars and pushed to the results array
-
-//if user choses to add special char
-// randomly select some number represented by Y number of chars and pushed to the results array
-
-// results = ["a", "b", "0", "?"]
-
-//display results onto the page
-
-//target text area, display results
+let userChoices = []
+if (lowerArr === true) {
+  userChoices = userChoices.concat(lowerArr)
+}
+  userChoices = userChoices.concat(numberArr)
+  userChoices = userChoices.concat(upperArr)
+  userChoices = userChoices.concat(specialArr)
 
 
-// Add event listener to generate button
+  console.log(userChoices);
+
+  // let password = ""; //what does the empty string do? 
+
+  //passing the data from the userInput, left-side is static and the property, the right-side in the value (in this case it's a variable).
+  
+
+
+for (let index = 0; index < password.length; index++) {
+  var randomIndex = Math.floor(Math.random() * userChoices.length);
+  password = password + userChoices[randomIndex];
+  
+}
+// starpasswordode Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Pseudo code
+// 1. Click on button to generate password
+//   -event listener - row generateBtn
+// 2. User wants ability to select password criteria - userInput function
+// 3. Options:
+//   -uppercase - confirm row
+//   -lowercase - confirm row
+//   -special characters - confirm row
+//   -numeric - confirm row
+//   -At least one character type must be chosen - alert row
+// 4.User wants to choose the length of the password - prompt row
+// 5. Set parameters on password length (8-128 characters) - prompt row
+// 6. Randomly select the characters based on user choices
+// 7. Display results on the page
